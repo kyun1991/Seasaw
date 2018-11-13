4f9748da19ca6d006ef89df5886010b56cce75f6
kyun1991 <38071517+kyun1991@users.noreply.github.com>
2018-11-13 15:03:13 +1300

    z

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+
+    public GameObject shell;
+    //public GameObject spawnPoint;
+
+    public LineRenderer lr;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Touch : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(GameControl.instance.shell, mousePosition, Quaternion.identity);
+        GameControl.instance.shell.GetComponent<Rigidbody2D>().isKinematic = true;
+        
+     
+        GameControl.instance.lr.enabled = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GameControl.instance.lr.enabled = false;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(GameControl.instance.shell, mousePosition, Quaternion.identity);
+        GameControl.instance.shell.GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+}
